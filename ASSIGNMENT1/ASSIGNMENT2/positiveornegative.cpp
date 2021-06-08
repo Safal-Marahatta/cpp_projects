cb601ed7dbebb73a17636ce72fbc12fc800fe6b0
Safal Marahatta <safalmarahatta123456@gmail.com>
2021-06-08 15:09:04 +0545

    conditional statement

---
+++ ASSIGNMENT1/ASSIGNMENT2/positiveornegative.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num;
+    cout <<"enter the number:"<<endl;
+    cin >> num;
+    if (num >= 0)
+    cout << num << "The number is positive.";
+    else
+    cout << num << "The number is negatiive. ";
+    return 0;
+
+}
